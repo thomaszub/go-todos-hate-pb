cbfd5e97ba4e77837f5ded7002a7d06c3715f999
Thomas Zub <thomas.zub@outlook.de>
2024-06-29 09:59:59 +0200

    feat: add db migration for todos collection

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+
+	_ "github.com/thomaszub/go-todos-hate-pb/migrations"
 	"github.com/thomaszub/go-todos-hate-pb/service"
 	"github.com/thomaszub/go-todos-hate-pb/ui"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: false,
+	})
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		s := service.NewToDos(e.App.Dao(), e.App.Logger())
---
+++ migrations/1_todos.go
@@ -0,0 +1,67 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `[
+			{
+				"id": "2u23enfabtxwxpe",
+				"created": "2024-06-27 18:47:01.812Z",
+				"updated": "2024-06-27 18:47:01.812Z",
+				"name": "todos",
+				"type": "base",
+				"system": false,
+				"schema": [
+					{
+						"system": false,
+						"id": "7vsmegan",
+						"name": "content",
+						"type": "text",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {
+							"min": null,
+							"max": null,
+							"pattern": ""
+						}
+					},
+					{
+						"system": false,
+						"id": "mezrknnb",
+						"name": "done",
+						"type": "bool",
+						"required": false,
+						"presentable": false,
+						"unique": false,
+						"options": {}
+					}
+				],
+				"indexes": [],
+				"listRule": null,
+				"viewRule": null,
+				"createRule": null,
+				"updateRule": null,
+				"deleteRule": null,
+				"options": {}
+			}
+		]`
+
+		collections := []*models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collections); err != nil {
+			return err
+		}
+
+		return daos.New(db).ImportCollections(collections, false, nil)
+	}, func(db dbx.Builder) error {
+		return nil
+	})
+}
